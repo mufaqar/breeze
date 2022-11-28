a30e471ad05d7e5aea47a2a3efb8ca979825bc8e
Mufaqar Islam <mufaqar@gmail.com>
2022-11-28 19:08:35 +0500

    Header

--- app/components/header.js
+++ app/components/header.js
@@ -1,8 +1,5 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import React from "react";
 
 export default function Header() {
-  return (
-    
-  )
+  return <>Header</>;
 }
