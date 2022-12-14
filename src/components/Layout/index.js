72fcff5d087eb94481d31c8cba6af2c283a494a6
syed-zunair-gillani <gillanix007@gmail.com>
2022-12-14 20:09:19 +0500

    video background

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,45 +1,37 @@
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { usePathname } from "next/navigation";
 import { useSelector } from "react-redux";
 import { darkTheme, lightTheme } from "../../theme/themes";
-
+import Header from "../header";
 
 export default function Layout(props) {
   const backgroud_image = useSelector((state) => state.changeBackground.backgroundImageURL);
-  // console.log("🚀 ~ file: index.js:7 ~ Layout ~ backgroud_image", backgroud_image);
-  const useDarkTheme = useSelector((state)=>state.swithDarkmode.darkMode)
-
-
-  const Videobackground = () => {
-    return (
-      <div class="bg-video-wrap">
-        <video
-          src="https://video-previews.elements.envatousercontent.com/files/2ce8819e-7dd4-40a2-9ad9-4720e4a9d90b/video_preview_h264.mp4"
-          loop
-          muted
-          autoplay
-        ></video>
-        <div class="overlay"></div>
-        <h1>Fullscreen video background</h1>
-      </div>
-    );
-  };
+  console.log("🚀 ~ file: index.js:7 ~ Layout ~ backgroud_image", backgroud_image);
+  const useDarkTheme = useSelector((state) => state.swithDarkmode.darkMode);
 
   return (
     <ThemeProvider theme={useDarkTheme ? darkTheme : lightTheme}>
-    <CssBaseline />
-    <Box
-      component="main"
-      style={{ background: backgroud_image.src ? `url(${backgroud_image.src})` : backgroud_image }}
-      className={`h-screen relative w-full _mianBgProperties px-16`}
-    >
-      {props.children}
-    </Box>
+      <CssBaseline />
+      {backgroud_image.src ? (
+        <Box
+          component="main"
+          style={{ background: backgroud_image.src ? `url(${backgroud_image.src})` : backgroud_image }}
+          className={`h-screen relative w-full _mianBgProperties px-16`}
+        >
+          {props.children}
+        </Box>
+      ) : (
+        <section id="main">
+          <video
+            // src={backgroud_image}
+            src="https://res.cloudinary.com/oscarjite/video/upload/v1662154978/walking_on_the_moon_j5tytl.mp4"
+            muted
+            autoplay
+            loop
+          ></video>
+          <div class="main-text px-16">{props.children}</div>
+        </section>
+      )}
     </ThemeProvider>
-    // <>
-    //   <div class="bloc-video">
-    //     <video autoplay muted loop src="https://assets.codepen.io/6093409/river.mp4" type="video/mp4"></video>
-    //     <div class="content-video">{props.children}</div>
-    //   </div>
-    // </>
   );
 }
